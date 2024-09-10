8561efae894452da7979ec2150b99aa5d6f5dcc3
AleksandrKompanetc <moneymacker7@gmail.com>
2024-09-10 23:41:01 +0300

    adding data

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Product() {
   return (
-    <div>Product!</div>
+    <div className='border py-2 px-4 rounded flex flex-col items-center mb-2'>Product!</div>
   )
 }
 
