639eb3746e9566ffeadc0dfa821ac6086b1b4b3b
AleksandrKompanetc <moneymacker7@gmail.com>
2024-09-10 23:52:07 +0300

    adding models

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function Product() {
+export function Product(props: any) {
   return (
     <div className='border py-2 px-4 rounded flex flex-col items-center mb-2'>Product!</div>
   )
