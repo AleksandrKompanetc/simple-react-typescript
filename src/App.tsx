32fa276eb208a4b03c466026c8350301851fa4e9
AleksandrKompanetc <moneymacker7@gmail.com>
2024-09-11 22:44:56 +0300

    adding hook useProduct

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,16 @@
-import React from 'react';
-import { Product } from './components/Product';
-import { products } from './data/products';
+import { Product } from "./components/Product";
+import { useProducts } from "./hooks/products";
 
 function App() {
+  const {loading, products, error} = useProducts();
+  
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      <Product product={products[0]} />
+      {loading && <p className='text-center'>Loading...</p>}
+      {error && <p className='text-center text-red-600'>{error}</p>}
+      {products.map(product => <Product product={product} key={product.id} />)}
     </div>
   )
 }
 
 export default App;
-
-
-// import { IProduct } from "../models";
-
-// export const products: IProduct[] = [
-//   {
-//     "id":1,
-//     "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//     "price":109.95,
-//     "description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-//     "category":"men's clothing",
-//     "image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-//     "rating":{
-//       "rate":3.9,
-//       "count":120
-//     }
-//   },
-//   {
-//     "id":2,
-//     "title":"Mens Casual Premium Slim Fit T-Shirts ",
-//     "price":22.3,
-//     "description":"Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-//     "category":"men's clothing",
-//     "image":"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-//     "rating":{
-//       "rate":4.1,
-//       "count":259
-//     }
-//   }
-// ]
